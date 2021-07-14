9e790667671b7c2c70d3669e43bfb84113f0741b
Onur Onat Surmeli <onatsurmeli@gmail.com>
2021-07-14 13:11:38 +0200

    -Fix linter issues. -Change main container appearance.

--- src/index.js
+++ src/index.js
@@ -1,37 +1,37 @@
-import _ from 'lodash';
 import './style.css';
-class Todo{
-  constructor(description = "", completed = false, index = 0){
+
+class Todo {
+  constructor(description = '', completed = false, index = 0) {
     this.description = description;
-    this.completed = completed; 
+    this.completed = completed;
     this.index = index;
   }
+
   add() {
-    let temp = document.getElementById("Container");
-    let obj = document.createElement('li');
-    let checkbox = document.createElement('input');
-    checkbox.classList.add('form-check-input', 'me-1');
-    checkbox.type = "checkbox";
-    checkbox.value = "";
-    checkbox.ariaLabel = "...";
+    const temp = document.getElementById('Container');
+    const obj = document.createElement('li');
+    const checkbox = document.createElement('input');
+    checkbox.classList.add('form-check-input');
+    checkbox.type = 'checkbox';
+    checkbox.value = '';
+    checkbox.ariaLabel = '...';
     obj.id = this.index;
-    obj.classList.add('list-group-item', 'list-item-style', 'ml-3')
+    obj.classList.add('list-group-item', 'list-item-style', 'mx-2');
     obj.appendChild(checkbox);
-    obj.innerHTML = obj.innerHTML + this.description;
-    if(this.completed){
+    obj.innerHTML += this.description;
+    if (this.completed) {
       obj.classList.add('disabled');
     }
     temp.appendChild(obj);
   }
-
 }
 
-let first = new Todo("clean the house", false, 0);
-let second = new Todo("take out the trash", true, 1);
-let third = new Todo ("go to toilet", true, 2);
-let arr = [first, second, third];
+const first = new Todo('clean the house', false, 0);
+const second = new Todo('take out the trash', true, 1);
+const third = new Todo('go to toilet', true, 2);
+const arr = [first, second, third];
 function printList(list) {
-  for(let i = 0; i < list.length; i += 1){
+  for (let i = 0; i < list.length; i += 1) {
     list[i].add();
   }
 }
