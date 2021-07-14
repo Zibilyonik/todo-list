26bb7fc16ebed92fdd4894b19a7fc3632b287e06
Onur Onat Surmeli <onatsurmeli@gmail.com>
2021-07-14 12:10:17 +0200

    -Add Todo class to index.js -Add container function into Todo class -Add printList function into index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 import _ from 'lodash';
 import './style.css';
+class Todo{
+  constructor(description = "", completed = false, index = 0){
+    this.description = description;
+    this.completed = completed; 
+    this.index = index;
+  }
+  add(container) {
+    let temp = document.getElementById(container);
+    let obj = document.createElement('li');
+    let checkbox = document.createElement('input');
+    checkbox.classList.add('form-check-input me-1');
+    checkbox.type = "checkbox";
+    checkbox.value = "";
+    checkbox.ariaLabel = "...";
+    obj.id = this.index;
+    obj.classList.add('list-group-item', 'list-item-style')
+    obj.appendChild(checkbox);
+    obj.innerText = this.description;
+    if(this.completed){
+      obj.classList.add('disabled');
+    }
+    temp.appendChild(obj);
+  }
 
-function component() {
-  const element = document.createElement('div');
-
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
 }
 
-document.body.appendChild(component());
+let first = new Todo("clean the house", false, 0);
+let second = new Todo("take out the trash", true, 1);
+let third = new Todo ("go to toilet", true, 2);
+let arr = [first, second, third];
+function printList(list) {
+  let container = document.getElementById("Container");
+  for(let i = 0; i < list.length; i += 1){
+    list[i].add(container);
+  }
+}
+printList(arr);
