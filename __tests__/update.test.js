f260427a4812d9ba0372fcf54e4503c6ece16a3f
Zibilyonik <onatsurmeli@gmail.com>
2021-07-23 15:35:44 +0200

    -Write unit test suite for changeStatus function.

--- __tests__/update.test.js
+++ __tests__/update.test.js
@@ -61,11 +61,35 @@ describe('delete completed task', () => {
   afterAll(() => {
     window.localStorage.setItem({});
   });
-  it('should return correct array lenght', () => {
+  it('should return correct array length', () => {
     expect(arr.length).toBe(1);
   });
 
   it('should return the remaining element', () => {
     expect(arr[0].completed).toBe(false);
+  });
+});
+
+describe('delete completed task', () => {
+  let arr = [];
+  document.body.innerHTML = '<div class="d-flex rounded col-6 col-lg-3 mx-auto mt-5 p-0 mb-1"><input type = "text" id="TodoDesc" class = "rounded col-9 p-0 m-0 text-center" placeholder="What to do?"><button type="button" class="button-style btn col-3 m-0" id="SubmitButton">Submit</button></div><ul id="Container" class="main-container list-group list-group-flush rounded col-6 col-lg-3 mb-1 mx-auto p-0"><li id="1"><p id="1-text">test</p></li></ul><div class="d-flex rounded col-6 col-lg-3 mx-auto p-0 mb-1"><button type="button" class="button-style btn col m-0" id="ClearButton">Delete All Completed Tasks</button></div>';
+  const item = document.getElementById('1');
+  beforeAll(() => {
+    Object.defineProperty(window, 'localStorage', {
+      value: fakeLocalStorage(),
+    });
+    arr.push({ description: 'test1', completed: false, index: 1 });
+    changeStatus(arr, 1);
+    arr = JSON.parse(window.localStorage.getItem('todoArray'));
+  });
+  afterAll(() => {
+    window.localStorage.setItem({});
+  });
+  it('should change completed status on array', () => {
+    expect(arr[0].completed).toBe(true);
+  });
+
+  it('should add disabled class to item', () => {
+    expect(item.classList.contains('disabled')).toBe(true);
   });
 });
