4b884482e29ad10ea642dd703544e593e4b6b716
moise10r <moiserushanika2006@gmail.com>
2021-07-23 15:27:08 +0200

    Add the delete completed test.

--- __tests__/update.test.js
+++ __tests__/update.test.js
@@ -46,3 +46,26 @@ describe('check for edit function', () => {
     expect(arr[0].description).toBe('tester');
   });
 });
+
+describe('delete completed task', () => {
+  let arr = [];
+  document.body.innerHTML = '<div class="d-flex rounded col-6 col-lg-3 mx-auto mt-5 p-0 mb-1"><input type = "text" id="TodoDesc" class = "rounded col-9 p-0 m-0 text-center" placeholder="What to do?"><button type="button" class="button-style btn col-3 m-0" id="SubmitButton">Submit</button></div><ul id="Container" class="main-container list-group list-group-flush rounded col-6 col-lg-3 mb-1 mx-auto p-0"><li id="1"><p id="1-text">test</p></li></ul><div class="d-flex rounded col-6 col-lg-3 mx-auto p-0 mb-1"><button type="button" class="button-style btn col m-0" id="ClearButton">Delete All Completed Tasks</button></div>';
+  beforeAll(() => {
+    Object.defineProperty(window, 'localStorage', {
+      value: fakeLocalStorage(),
+    });
+    arr.push({ description: 'test1', completed: true, index: 1 }, { description: 'test2', completed: false, index: 2 });
+    deleteMarked(arr);
+    arr = JSON.parse(window.localStorage.getItem('todoArray'));
+  });
+  afterAll(() => {
+    window.localStorage.setItem({});
+  });
+  it('should return correct array lenght', () => {
+    expect(arr.length).toBe(1);
+  });
+
+  it('should return the remaining element', () => {
+    expect(arr[0].completed).toBe(false);
+  });
+});
